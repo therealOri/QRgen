53a0e0b8be8a6ce21c03e5179065459e4ceb7aee
Ori <45724082+therealOri@users.noreply.github.com>
2021-01-23 11:27:50 -0700

    comments
    Added developer comments to the code for better readability and to help with understanding with what can be changed and what some things can be done.

--- QRgen.py
+++ QRgen.py
@@ -2,9 +2,9 @@ import qrcode
 
 
 qr = qrcode.QRCode(version=1,
-                   error_correction=qrcode.constants.ERROR_CORRECT_L,
-                   box_size=50,
-                   border=2)
+                   error_correction=qrcode.constants.ERROR_CORRECT_L,#Don't change this.
+                   box_size=50,#This changes how big the code will be.
+                   border=2) #This changes how big/thicc the border of the qrcode will be.
 
 print("What text/url would you like to have be turned into a QR code?: ")
 data = input()
@@ -12,6 +12,6 @@ data = input()
 print("Converting...")
 img = qrcode.make(data)
 print("Done!")
-print("\nMake sure to move code.png to another folder before making a new QRcode!")
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("code.png")
+print("\nMake sure to move code.png to another folder and rename it to whatever before making a new QRcode!")
+img = qr.make_image(fill_color="black", back_color="white") #This changes the qrcode's look/what colors are used.
+img.save("code.png") #This name can be changed to whatever you want.
